b0c984113b9fd053a3e9a73a46d6258aa6397081
Pradeep Chand <prad_178@yahoo.com>
2013-08-12 00:12:44 -0400

    first django app - part 2 complete

--- polls/admin.py
+++ polls/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
 from polls.models import Poll, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class PollAdmin(admin.ModelAdmin):
     #fields = ['pub_date', 'question']
+    #Adding a filter by pub_date
+    list_filter = ['pub_date']
+    
+    #List of items to be displayed
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    
+    #Adding a search field
+    search_fields = ['question']
+    
+    #Adding a breadcrumb for date hierarchy
+    date_hierarchy = 'pub_date'
+    
     fieldsets = [
         (None,                  {'fields' : ['question']}),
         ('Date information',    {'fields' : ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
+
+#admin.site.register(Choice)
--- pradeepchand/settings.py
+++ pradeepchand/settings.py
@@ -108,6 +108,7 @@ ROOT_URLCONF = 'pradeepchand.urls'
 WSGI_APPLICATION = 'pradeepchand.wsgi.application'
 
 TEMPLATE_DIRS = (
+    "/home/prad/projects/pradeepchand/templates"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
