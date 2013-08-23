25398e8024a45297f50bba6ab92f5c04b55da7d1
Pradeep Chand <prad_178@yahoo.com>
2013-08-22 23:42:53 -0400

    Chpt 5 complete

--- polls/tests.py
+++ polls/tests.py
@@ -70,5 +70,15 @@ class PollViewTests(TestCase):
             response.context['latest_poll_list'], 
             ['<Poll: Past poll.>']
         )
-    
+
+class PollIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_poll(self):
+        future_poll = create_poll(question="Future poll.", days=5)
+        response = self.client.get(reverse('polls:detail', args=(future_poll.id,)))
+        self.assertEqual(response.status_code, 404)
+        
+    def test_detail_view_with_a_past_poll(self):
+        past_poll = create_poll(question='Past poll.', days = -5)
+        response = self.client.get(reverse('polls:detail', args=(past_poll.id,)))
+        self.assertContains(response, past_poll.question, status_code=200)
     
